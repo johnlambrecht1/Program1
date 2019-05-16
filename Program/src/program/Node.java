1b2064afeab0217903e095a74e0b08c465b45c3a
John Lambrecht <jclambrecht97@msn.com>
2019-05-16 09:06:31 -0600

    Added node class

--- Program/src/program/Node.java
+++ Program/src/program/Node.java
@@ -1,7 +1,30 @@
 package program;
 
 public class Node {
-    Node(){
-
+    Node parent;
+    Node leftChild;
+    Node rightChild;
+    private int key;
+    public Node(int input){
+        System.out.println("node created");
+        key = input;
+    }
+    public void setRightChild (Node temp){
+        rightChild = temp;
+    }
+    public void setLeftChild (Node temp){
+        leftChild = temp;
+    }
+    public void setParent (Node temp){
+        parent = temp;
+    }
+    public Node getParent (){
+        return parent;
+    }
+    public Node getLeftChild(){
+        return leftChild;
+    }
+    public Node getRightChild(){
+        return rightChild;
     }
 }
