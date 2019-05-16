bb4acb8c1a56947531ed201aa86bbee665f2038c
John Lambrecht <jclambrecht97@msn.com>
2019-05-16 12:09:58 -0600

    added tree file

--- Program/src/program/Program.java
+++ Program/src/program/Program.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
+        tree mytree;
         Scanner sc;
         try {
             sc = new Scanner(new File("input.txt"));
@@ -22,6 +23,11 @@ public class Program {
                 for (int i = 0; i < splitstring.length; i++) {
                     split[i] = Integer.parseInt(splitstring[i]);
                     System.out.println(split[i]);
+                }
+                int j = 0;
+                mytree = new tree();
+                while (j<split.length){
+                    mytree.buildTree(split[j]);
                 }
             }
         } catch (FileNotFoundException e) {
---
+++ Program/src/program/tree.java
@@ -0,0 +1,14 @@
+package program;
+
+public class tree {
+    Node parent;
+    Node rightChild;
+    Node leftChild;
+    tree(){
+        parent = null;
+    }
+
+    void buildTree(int key){
+
+    }
+}
